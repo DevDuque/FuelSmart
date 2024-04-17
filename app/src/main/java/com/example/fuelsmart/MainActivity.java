1d90cf133e80b6fa06efe4193aacad9fada8fb07
DevDuque <devgduque@gmail.com>
2024-04-17 13:10:59 -0300

    Add second page & Bugged buttons

--- app/src/main/java/com/example/fuelsmart/MainActivity.java
+++ app/src/main/java/com/example/fuelsmart/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.fuelsmart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void alert(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    private View.OnClickListener onClickSubmit() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText alcoholInput = (EditText) findViewById(R.id.input_AlcoholPrice);
+                EditText gasInput = (EditText) findViewById(R.id.input_GasPrice);
+
+                String alcoholPrice = alcoholInput.getText().toString();
+                String gasPrice = gasInput.getText().toString();
+
+                // Garantindo que os campos foram inseridos
+                if(!alcoholPrice.isEmpty() || !gasPrice.isEmpty()) {
+                    // Navegando para próxima tela
+                    Intent nextPage = new Intent(getBaseContext(), ConsumePageActivity.class);
+
+                    // Salvando os preços
+                    Bundle prizes = new Bundle();
+                    prizes.putString("alcoholPrize", alcoholPrice);
+                    prizes.putString("gasPrize", gasPrice);
+                    nextPage.putExtras(prizes);
+
+                } else {
+                    alert("Por favor insira um valor válido para os campos");
+                }
+            }
+        };
     }
 }
